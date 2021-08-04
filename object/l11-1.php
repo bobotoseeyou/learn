9f42670f5d1a175bed5ae7a3b837563e62773a8d
gaobo123 <371017982@qq.com>
2021-08-04 09:43:10 +0800

    优化页面

--- object/l11-1.php
+++ object/l11-1.php
@@ -1,13 +1,28 @@
 <?php
 
-Class A{
+Class A extends C{
     public $p1 = 1;
+    function f1(){
+        echo "\n 当前对象的类".get_class($this);
+        echo "\n p1的值：".$this->p1;
+        @B::f2();
+        $this->f3();
+    }
 }
 
 Class B{
-
+    function f2(){
+//        echo "\n p1的值：";
+        echo "\n 当前对象的类".get_class($this);
+        echo "\n p1的值：".$this->p1;
+    }
 }
 
-Class B{
-
+Class C{
+    function f3(){
+        echo "\n 当前对象的类".get_class($this);
+        echo "\n p1的值：".$this->p1;
+    }
 }
+$o = new A();
+$o->f1();
